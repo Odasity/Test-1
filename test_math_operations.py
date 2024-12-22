db761e7c9449f2087be88d2b27d3df5dbdb1054e
Odasity <amitabhdas.p@gmail.com>
2024-12-21 20:12:53 -0600

    test_math_operation edits

--- test_math_operations.py
+++ test_math_operations.py
@@ -1,1 +1,25 @@
+#day2 chapGPT's 10 day automation preparation course
+#unittest exercise
 
+import unittest
+
+class TestMathOperations(unittest.TestCase):
+    def setUp(self):
+        print("Setting up resources")
+        
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+
+    def test_subtraction(self):
+        self.assertEqual(5 - 3, 2)
+
+    def test_multiplication(self):
+        self.assertNotEqual(2 * 3, 5)
+        self.assertIn('apple', ['apple', 'banana'])
+
+    def tearDown(self):
+        print("Cleaning up resources")
+
+
+if __name__ == '__main__':
+    unittest.main()
